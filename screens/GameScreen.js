26c6b1ce831b4a8a255500436f611a5dfa5afd92
Cankat Örüm <52240938+cankat-orum@users.noreply.github.com>
2021-08-30 17:24:49 +0300

    GameScreen styling
    - random number generation function

---
+++ screens/GameScreen.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+
+import NumberCon from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}; // generates a num between a given range (excludes given number)
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent Guess</Text>
+      <NumberCon>{currentGuess}</NumberCon>
+      <Card style={styles.buttonCon}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonCon: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
+
+export default GameScreen;
